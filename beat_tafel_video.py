f149fe180a370dc6e2f2e7bb5a0150245a25df6a
NiklasWehl <niklaswehl@outlook.de>
2020-12-08 14:32:27 +0100

    änderung

--- beat_tafel_video.py
+++ beat_tafel_video.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 
+
+#cap = cv2.VideoCapture(0)
 img = cv2.imread("BespielFeld.png")
 cv2.imshow("BespielFeld.png", img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+print("test")
