d69a40e73ff043bd949244ac400127690df44511
NiklasWehl <niklaswehl@outlook.de>
2021-01-05 13:40:15 +0100

    midi update

--- test sortierung.py
+++ test sortierung.py
@@ -7,11 +7,11 @@ import mido
 # Hilfsvariable
 frameAuslesen = True
 colorListeIndex = 0
-#frameKalibierung = True
 taktCode = []
 feldNummerListe = []
 ausgabeListe = ["","","","","","","","","","","","","","","",""]
 
+# MIDI-Output suchen
 print("Midi output ports: ", mido.get_output_names())
 midiOutput = mido.open_output("LoopBe Internal MIDI 2")
 
@@ -25,10 +25,12 @@ cap = cv2.VideoCapture('beat_tafel.mp4')
 # Live-Video
 #cap = cv2.VideoCapture(0)
 
+# Senden der Infomationen über MIDI
 def sendControlChange(x):
     x = int(x)
     message = mido.Message('control_change', control=3, value=x)
     midiOutput.send(message)
+
 
 # Bestimmen der verschiedenen Regionen einer Farbe
 def regionErmitteln (index, countoursRange):
@@ -36,12 +38,9 @@ def regionErmitteln (index, countoursRange):
     maximumRange = max(countoursRange) + 1
 
     indexRange = list(range(minimumRange, maximumRange))
-    #print(str(indexRange[index]) + " index")
     del indexRange[index]
 
-    #print(indexRange)
     return (indexRange)
-
 
 
 # Jedem Feld eine eindeutige Bezeichung zuordnen
@@ -66,17 +65,14 @@ def feldNummer(maskeFarbe, contours):
         else:
             cYRegion = int(M["m01"] / M["m00"])
             cv2.circle(median, (cXRegion, cYRegion), 5, (255, 0, 255), -1)
-        #print("Y-Wert: " + str(cYRegion))
-        #print("X-Wert: " + str(cXRegion))
 
         colorStr = str(colorNumb) + "." + str(cYRegion) + "." + str(cXRegion)
-        #print(colorStr)
         colorStrListe.append(colorStr)
 
     return(colorStrListe)
 
 
-
+# Einsotiernen der Farbfelder in die richtige Position
 def trackCode (Liste):
     for i in Liste:
         for feldCode in i:
@@ -225,6 +221,7 @@ def trackCode (Liste):
                     ausgabeListe[7] = colorCode
                 '''    
 
+    # leere Elemente mit 0en auffüllen
     listeCount = 0
     for i in ausgabeListe:
         
@@ -236,14 +233,13 @@ def trackCode (Liste):
             ausgabeListe[listeCount] = 0
             listeCount += 1
 
+    # Jedes Elemente der sortierten Liste in 
     for i in ausgabeListe:
         print(i)
         sendControlChange(i)         
             
 
     print(ausgabeListe)
-
-
 
 
 
@@ -257,36 +253,41 @@ while cap.isOpened() and frameAuslesen == True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     h,s,v = cv2.split(hsv)
 
+    # Länge der Liste 
     colorElemente = len(colorListe)
-    #print(colorElemente)
 
+    # Nach jeder einzelenen Farbe suchen
     while colorListeIndex < colorElemente:
-
+        
+        # Liste in 3er Schritten abrufen
         colorNumb = colorListe[colorListeIndex]
-        #print(colorListeIndex)
+
+        # Nach Farbe suchen
         hueRange = cv2.inRange(h, colorListe[colorListeIndex] - 10, colorListe[colorListeIndex] + 10)
         satRange = cv2.inRange(s, 50, 255)
 
+        # Masken multiplizieren
         maskeFarbe = cv2.multiply(hueRange,satRange)
 
+        # Median bilden
         ksize = 11
         median = cv2.medianBlur(maskeFarbe, ksize)
-        #cv2.imshow('Video_Masken', median)
+        cv2.imshow('Video_Masken', median)
         
-
+        # Regionen finden
         contours,hierarchy=cv2.findContours(median,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
         feldNummerListeFarbe = feldNummer(colorNumb, contours)
-        
-        #print(feldNummerListeFarbe)
 
         feldNummerListe.append(feldNummerListeFarbe)
 
+        # nachste Farbe bearbeiten
         colorListeIndex += 3 
 
         if cv2.waitKey(25) != -1:
             break
-        #time.sleep(1)
+        time.sleep(1)
+
 
     if frameAuslesen == True:
         
@@ -294,7 +295,7 @@ while cap.isOpened() and frameAuslesen == True:
     
     trackCode(feldNummerListe)
   
-
+    # Auslesen des Frames nicht erneut durchführen
     frameAuslesen = False
   
 
